f8fffb2614d33ad8c0dfe314fd9e82586f9ce634
sunandaunni-eaton <89196514+sunandaunni-eaton@users.noreply.github.com>
2024-06-06 19:57:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ df = my_catalog.to_pandas()
 # streamlit.write(df)
 # put the first column into a list
 #color_list = df[0].values.tolist()
-print(df)
+print(df['COLOR_OR_STYLE'])
 session.stop()
 # Let's put a pick list here so they can pick the color
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
